f7d8f7c855cd9e1610911068a6e8f284ba305a91
Sergio Acebes y Jorge Jaular <acebes9@gmail.com>
2018-02-07 14:01:33 +0000

    Ahora genera mensajes aleatorios durante la conversacion.

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,23 @@
  */
 public class Responder
 {
+    Random numAleatorio;
+    ArrayList<String> respuesta;
+        
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        numAleatorio = new Random();
+        respuesta = new ArrayList<>();
+        respuesta.add("Le escucho");
+        respuesta.add("Le pasare con el servicio tecnico,espere un segundo gracias");
+        respuesta.add("Ya estamos trabajando en ello");
+        respuesta.add("¿Algo más que necesite saber?");
+        respuesta.add("Siga contandome mientras trato de solucionarlo");
+        respuesta.add("¿Se enciende una luz en el lateral?");
+        respuesta.add("Ahora debe reiniciar el equipo para que pueda seguir arreglandole el problema");
     }
 
     /**
@@ -20,6 +34,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int num = numAleatorio.nextInt(6);
+        return respuesta.get(num);
     }
 }
